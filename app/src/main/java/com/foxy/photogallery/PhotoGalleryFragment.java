f11c41a63e8f1595bbe5ab74d7e5b2874ef10beb
Olesya Gorbacheva <554476744j@gmail.com>
2019-10-01 17:51:52 +0800

    добавлен holder, adapter, postExecute в AsyncTask

--- app/src/main/java/com/foxy/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/foxy/photogallery/PhotoGalleryFragment.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,12 +15,15 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PhotoGalleryFragment extends Fragment {
 
     private static final String TAG = "PhotoGalleryFragment";
 
     private RecyclerView recyclerView;
+    private List<GalleryItem> items = new ArrayList<>();
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
@@ -28,8 +32,8 @@ public class PhotoGalleryFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setRetainInstance(true);         // удержание фрагмента
-        new FetchItemsTask().execute();  // апуск фонового потока
+        setRetainInstance(true);              // удержание фрагмента
+        new FetchItemsTask(items).execute();  // запуск фонового потока
     }
 
     @Nullable
@@ -38,16 +42,76 @@ public class PhotoGalleryFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
         recyclerView = view.findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        setupAdapter();
         return view;
     }
 
+    private void setupAdapter() {
+        // проверка на то, что фрагмент был присоединен к activity и что getActivity() != null
+        // необходимо из-за использования AsyncTask
+        if (isAdded()) {
+            recyclerView.setAdapter(new PhotoAdapter(items));
+        }
+    }
 
-    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+
+    private class PhotoHolder extends RecyclerView.ViewHolder {
+        private TextView titleTextView;
+
+        public PhotoHolder(@NonNull View itemView) {
+            super(itemView);
+            titleTextView = (TextView) itemView;
+        }
+
+        public void bind(GalleryItem item) {
+            titleTextView.setText(item.toString());
+        }
+    }
+
+    private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
+        private List<GalleryItem> galleryItems;
+
+        public PhotoAdapter(List<GalleryItem> galleryItems) {
+            this.galleryItems = galleryItems;
+        }
+
+        @NonNull
+        @Override
+        public PhotoHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            TextView textView = new TextView(getActivity());
+            return new PhotoHolder(textView);
+        }
 
         @Override
-        protected Void doInBackground(Void... voids) {
-            new FlickrFletchr().fetchItems();
-            return null;
+        public void onBindViewHolder(@NonNull PhotoHolder holder, int position) {
+            GalleryItem item = galleryItems.get(position);
+            holder.bind(item);
+        }
+
+        @Override
+        public int getItemCount() {
+            return galleryItems.size();
+        }
+    }
+
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, List<GalleryItem>> {
+        private List<GalleryItem> items;
+
+        public FetchItemsTask(List<GalleryItem> items) {
+            this.items = items;
+        }
+
+        @Override
+        protected List<GalleryItem> doInBackground(Void... voids) {
+            return new FlickrFletchr().fetchItems();
+        }
+
+        @Override
+        protected void onPostExecute(List<GalleryItem> items) {
+            this.items = items;
+            setupAdapter();
+            Log.i(TAG, "GalleryItems size: " + items.size());
         }
     }
 }
