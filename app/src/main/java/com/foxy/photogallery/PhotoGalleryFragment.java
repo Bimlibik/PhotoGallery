fcca9c25c71e0fac65fb259cd3c19f4dcb290470
Olesya Gorbacheva <554476744j@gmail.com>
2019-10-04 12:54:00 +0800

    .

--- app/src/main/java/com/foxy/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/foxy/photogallery/PhotoGalleryFragment.java
@@ -14,7 +14,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.QuickContactBadge;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
