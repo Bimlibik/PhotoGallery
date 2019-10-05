a54168723692a19ba353ca637c13e0193b5179b2
Olesya Gorbacheva <554476744j@gmail.com>
2019-10-05 15:24:55 +0800

    добавлены уведомления

--- app/src/main/java/com/foxy/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/com/foxy/photogallery/PhotoGalleryActivity.java
@@ -1,12 +1,18 @@
 package com.foxy.photogallery;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
 
 public class PhotoGalleryActivity extends SingleFragmentActivity {
 
+    // запуск activity
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PhotoGalleryActivity.class);
+    }
+
+    // запуск фрагмента
     @Override
     protected Fragment createFragment() {
         return PhotoGalleryFragment.newInstance();
--- app/src/main/java/com/foxy/photogallery/PollService.java
+++ app/src/main/java/com/foxy/photogallery/PollService.java
@@ -2,14 +2,18 @@ package com.foxy.photogallery;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.os.SystemClock;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -18,7 +22,7 @@ public class PollService extends IntentService {
     private static final String TAG = "PollService";
 
     // 60 секунд
-    private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(1);
+    private static final long POLL_INTERVAL_MS = TimeUnit.MINUTES.toMillis(15);
 
     public PollService() {
         super(TAG);
@@ -77,6 +81,22 @@ public class PollService extends IntentService {
             Log.i(TAG, "Got an old result: " + resultId);
         } else {
             Log.i(TAG, "Got a new result: " + resultId);
+
+            Resources resources = getResources();
+            Intent i = PhotoGalleryActivity.newIntent(this);
+            PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setTicker(resources.getString(R.string.new_pictures_title))        // текст бегущей строки
+                    .setSmallIcon(android.R.drawable.ic_menu_report_image)              // иконка
+                    .setContentTitle(resources.getString(R.string.new_pictures_title))  // заголовок на выдвижной панели
+                    .setContentText(resources.getString(R.string.new_pictures_text))    // текст на выдвижной панлеи
+                    .setContentIntent(pi)                                               // что будет запускаться при нажатии
+                    .setAutoCancel(true)                                                // при нажатии на оповещение оно удаляется с панели
+                    .build();
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            notificationManager.notify(0, notification);   // id - идентификатор оповещения
         }
 
         QueryPreferences.setLastResultId(this, resultId);
