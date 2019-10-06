6f2283b9887679818a86cccd4ea45be2fa5a1af1
Olesya Gorbacheva <554476744j@gmail.com>
2019-10-06 15:35:27 +0800

    добавлен шаблон BroadcastReceiver'а

---
+++ app/src/main/java/com/foxy/photogallery/StartupReceiver.java
@@ -0,0 +1,16 @@
+package com.foxy.photogallery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+// Автономный BroadcastReceiver. Пробуждается при каждой отправке broadcast'а системы о включении устройства
+public class StartupReceiver extends BroadcastReceiver {
+    private static final String TAG = "StartupReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Received broadcast intent: " + intent.getAction());
+    }
+}
